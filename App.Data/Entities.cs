49bcf3055b8bd707f756ef23cfbfa9ecdd4ecc9a
Koistya `Navin <koistya@gmail.com>
2011-02-28 21:00:04 +0300

    MembershipService in progress.

---
+++ App.Data/Entities.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Entities.cs" company="RIA Library Foundation">
+//     Copyright © 2011 RIA Library Foundation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace App.Data
+{
+    using System.Data.Entity;
+    using App.Data.Models;
+
+    public class Entities : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ App.Data/Models/User.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="User.cs" company="RIA Library Foundation">
+//     Copyright © 2011 RIA Library Foundation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace App.Data.Models
+{
+    using System;
+
+    [Flags]
+    public enum UserRole
+    {
+        RegularUser = 0,
+        Administrator = 1,
+        Moderators = 2
+    }
+
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public UserRole Role { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime LastLoginDate { get; set; }
+
+        public DateTime LastActivityDate { get; set; }
+    }
+}
