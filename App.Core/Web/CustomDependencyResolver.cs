4ca9c3d9a06256dae4a4544e13f2e1b0cfc23219
Koistya `Navin <koistya@gmail.com>
2011-03-04 19:48:58 +0300

    CustomDependencyResolver added

---
+++ App.Core/Web/CustomDependencyResolver.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CustomApplication.cs" company="RIA Library Foundation">
+//     Copyright © 2011 RIA Library Foundation. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace App.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using Microsoft.Practices.Unity;
+
+    /// <summary>
+    /// Defines the methods that simplify service location and dependency resolution.
+    /// </summary>
+    public class CustomDependencyResolver : IDependencyResolver
+    {
+        private readonly IUnityContainer container;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomDependencyResolver"/> class.
+        /// </summary>
+        /// <param name="container">The Unity container.</param>
+        public CustomDependencyResolver(IUnityContainer container)
+        {
+            this.container = container;
+        }
+
+        /// <summary>
+        /// Resolves singly registered services that support arbitrary object creation.
+        /// </summary>
+        /// <param name="serviceType">The type of the requested service or object.</param>
+        /// <returns>The requested service or object.</returns>
+        public object GetService(Type serviceType)
+        {
+            return (serviceType.IsClass && !serviceType.IsAbstract) || container.IsRegistered(serviceType) ?
+                container.Resolve(serviceType) : null;
+        }
+
+        /// <summary>
+        /// Resolves multiply registered services.
+        /// </summary>
+        /// <param name="serviceType">The type of the requested services.</param>
+        /// <returns>The requested services.</returns>
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return (serviceType.IsClass && !serviceType.IsAbstract) || container.IsRegistered(serviceType) ?
+                container.ResolveAll(serviceType) : new object[] { };
+        }
+    }
+}
